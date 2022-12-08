134c652fe0764765bdd4559f6fa26321933525d2
Boniwayne <Boniwayne125@gmail.com>
2022-12-08 03:00:02 -0800

    made canges

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 	/**
-	 * main - Entry point
-	 *
-	 * Description:'A C program that prints with puts function'
-	 *
-	 * Return: Always 0 (Success)
+	 * main - prints the phrase '"Programming is like building a multilingual puzzle'
+	 * Return: 0 if exited properly, non-zero otherwise
 	 */
 	int main(void)
-	{
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);
-	}
+}
