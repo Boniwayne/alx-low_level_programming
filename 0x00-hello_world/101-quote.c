5dcc04c10bb491bba5286de885848d32c2089621
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-08 16:42:47 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-#include <unistd.h>
-
-
-
 /**
+ * main- Entry point
  *
- * main - prints exactly "and that piece of art is useful"
+ * Description: A program that prints to standard error
  *
- *  - Dora Korpar, 2015-10-19",
- *
- *   followed by a new line, to the standard error.
- *
- *   Return: Always 0 (Success)
- *
+ * Return: Always 1 (success)
  */
-
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
