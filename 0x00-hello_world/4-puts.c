448d2de58d6cd0a7970c213adfd5223aa4b9d721
Boniwayne <Boniwayne125@gmail.com>
2022-12-08 02:00:14 -0800

    Changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 	/**
+	 * main - Entry point
 	 *
-	 *  * main - Entry point
+	 * Description:'A C program that prints with puts function'
 	 *
-	 *   *
-	 *
-	 *    * Return: Always 0 (Success)
-	 *
+	 * Return: Always 0 (Success)
 	 */
-int main(void)
-{
-		char str[] = "\"Programming is like building a multilingual puzzle";
+	int main(void)
+	{
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
-			puts(str);
-				return (0);
-}
+	puts(str);
+	return (0);
+	}
