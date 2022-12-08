f5ae6bd6a1dd43aa52802c80587d03db85ebe1e4
Boniwayne <Boniwayne125@gmail.com>
2022-12-08 01:31:10 -0800

    Changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
- 
-int main(void)
+
 	/**
 	 *
 	 *  * main - Entry point
@@ -9,16 +8,11 @@ int main(void)
 	 *
 	 *    * Return: Always 0 (Success)
 	 *
-	 *     */
-
+	 */
+int main(void)
 {
-
 		char str[] = "\"Programming is like building a multilingual puzzle";
 
-
-
 			puts(str);
-
 				return (0);
-
 }
