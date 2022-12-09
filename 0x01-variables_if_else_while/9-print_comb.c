400579090ca1a2217c8f74e8861ab9cd21e63083
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-09 15:50:34 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,43 +1,22 @@
-i#include <stdio.h>
-
-
+#include <stdio.h>
 
 /**
- *
- *  * main - prints all possible combinations of single-digit numbers
- *
- *   * Return: Always 0 (Success)
- *
- *    */
-
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int n;
 
-		int n;
-
-
-
-			for (n = 48; n < 58; n++)
-
-					{
-
-								putchar(n);
-
-										if (n != 57)
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
