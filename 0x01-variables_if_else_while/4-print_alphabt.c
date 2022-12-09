b7e0a47032a780190725baada45dab29879579da
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-09 15:02:48 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints the alphabet in lowercase,
- *
- *   * followed by a new line, except q and e
- *
- *    * Return: Aways 0 (Sucess)
- *
- *     */
-
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
 int main(void)
-
 {
+	char ch = 'a';
 
-		char ch = 'a';
-
-
-
-			while (ch <= 'z')
-
-					{
-
-								if (ch != 'e' && ch != 'q')
-
-											{
-
-															putchar(ch);
-
-																	}
-
-										ch++;
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
