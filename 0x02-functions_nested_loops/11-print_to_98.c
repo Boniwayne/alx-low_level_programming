2edaa3ddbf6c5c05478b8c4339da0673a18e4d2e
Boniwayne <Boniwayne125@gmail.com>
2022-12-13 05:23:01 -0800

    Add task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,67 @@
+i#include <unistd.h>
+
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_to_98- Entry point
+ *
+ *   * @n: integer input for start point
+ *
+ *    *
+ *
+ *     * Description: Print all number within range n - 98
+ *
+ *      *
+ *
+ *       * Return: void
+ *
+ *        */
+
+void print_to_98(int n)
+
+{
+
+		int i;
+
+
+
+			i = n;
+
+				while (1)
+
+						{
+
+									if (i != 98)
+
+												{
+
+																printf("%d, ", i);
+
+																		} else
+
+																					{
+
+																									/* when i equals 98, break while loop */
+
+																									printf("%d\n", i);
+
+																												break;
+
+																														}
+
+																				if (i < 98)
+
+																								i++;
+
+																						if (i > 98)
+
+																										i--;
+
+																							}
+
+}
