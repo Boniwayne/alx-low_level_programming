d2818ae1d09eade72e7422fb940d17f7700e15fe
Boniwayne <Boniwayne125@gmail.com>
2022-12-13 04:34:37 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_last_digit- Entry point
+ *
+ *   *
+ *
+ *    * @i: integer parameter
+ *
+ *     *
+ *
+ *      * Description: Check modulus of integer
+ *
+ *       *
+ *
+ *        * Return: Always last digit of int
+ *
+ *         */
+
+int print_last_digit(int i)
+
+{
+
+		int ret_val;
+
+
+
+			if (i < 0)
+
+						ret_val = -1 * (i % 10);
+
+				else
+
+							ret_val = i % 10;
+
+					_putchar(ret_val + '0');
+
+						return (ret_val);
+
+}
