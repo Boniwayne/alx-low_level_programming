1e9be1e8fa56e5e632148c9778173f697dfec1e3
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-13 15:38:08 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,53 +1,27 @@
-i#include <unistd.h>
-
+#include <unistd.h>
 #include "main.h"
 
-
-
 /**
+ * jack_bauer- Entry point
  *
- *  * jack_bauer- Entry point
+ * Description: Print every minute in 24hours
  *
- *   *
- *
- *    * Description: Print every minute in 24hours
- *
- *     *
- *
- *      * Return: Always 0 (success)
- *
- *       */
-
+ * Return: Always 0 (success)
+ */
 void jack_bauer(void)
-
 {
+	int hour, minute;
 
-		int hour, minute;
-
-
-
-			for (hour = 0; hour < 24; hour++)
-
-					{
-
-								for (minute = 0; minute < 60; minute++)
-
-											{
-
-															_putchar((hour / 10) + 48);
-
-																		_putchar((hour % 10) + 48);
-
-																					_putchar(':');
-
-																								_putchar((minute / 10) + 48);
-
-																											_putchar((minute % 10) + 48);
-
-																														_putchar('\n');
-
-																																}
-
-									}
-
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((minute / 10) + 48);
+			_putchar((minute % 10) + 48);
+			_putchar('\n');
+		}
+	}
 }
