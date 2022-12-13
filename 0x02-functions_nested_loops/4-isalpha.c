db0ad2da24ff31e5ee686232d41138f0f6e3b710
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-13 15:15:38 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,39 +1,20 @@
-i#include <unistd.h>
-
+#include <unistd.h>
 #include <ctype.h>
-
 #include "main.h"
 
-
-
 /**
+ * _isalpha- Entry point
  *
- *  * _isalpha- Entry point
+ * @c: integer parameter
  *
- *   *
+ * Description: Check alphabet characters
  *
- *    * @c: integer parameter
- *
- *     *
- *
- *      * Description: Check alphabet characters
- *
- *       *
- *
- *        * Return: Always 1 or 0 (success)
- *
- *         */
-
+ * Return: Always 1 or 0 (success)
+ */
 int _isalpha(int c)
-
 {
-
-		if isalpha(c)
-
-					return (1);
-
-			else
-
-						return (0);
-
+	if isalpha(c)
+		return (1);
+	else
+		return (0);
 }
