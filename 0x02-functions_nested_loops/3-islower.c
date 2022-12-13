0da730e24ec1cdbff66e2bd79f039569b4720770
Boniwayne <Boniwayne125@gmail.com>
2022-12-13 04:09:35 -0800

    Changes made

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+#include <ctype.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _islower- Entry point
+ *
+ *   *
+ *
+ *    * @c: integer parameter
+ *
+ *     *
+ *
+ *      * Description: Check lowercase characters
+ *
+ *       *
+ *
+ *        * Return: Always 1 or 0 (success)
+ *
+ *         */
+
+int _islower(int c)
+
+{
+
+		if islower(c)
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
