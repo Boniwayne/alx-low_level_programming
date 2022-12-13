16051441bbb9cd3c54f3c8843ada75049c6bfdfe
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-13 15:41:51 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,69 +1,35 @@
 #include <unistd.h>
-
 #include "main.h"
 
-
-
 /**
+ * times_table- Entry point
  *
- *  * times_table- Entry point
+ * Description: Print 9 times table
  *
- *   *
- *
- *    * Description: Print 9 times table
- *
- *     *
- *
- *      * Return: Always 0 (success)
- *
- *       */
-
+ * Return: Always 0 (success)
+ */
 void times_table(void)
-
 {
+	int i, j, times;
 
-		int i, j, times;
-
-
-
-			for (i = 0; i <= 9; i++)
-
-					{
-
-								_putchar('0');
-
-										for (j = 1; j <= 9; j++)
-
-													{
-
-																	times = i * j;
-
-																				_putchar(',');
-
-																							_putchar(' ');
-
-																										if (times <= 9)
-
-																														{
-
-																																			_putchar(' ');
-
-																																							_putchar(times + 48);
-
-																																										} else
-
-																																														{
-
-																																																			_putchar((times / 10) + 48);
-
-																																																							_putchar((times % 10) + 48);
-
-																																																										}
-
-																																												}
-
-												_putchar('\n');
-
-													}
-
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			times = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (times <= 9)
+			{
+				_putchar(' ');
+				_putchar(times + 48);
+			} else
+			{
+				_putchar((times / 10) + 48);
+				_putchar((times % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
