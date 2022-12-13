6359b2b997e1404d19a3fc8b26f355bb654bd679
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-13 15:05:22 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,51 +1,26 @@
 #include <unistd.h>
-
 #include "main.h"
 
-
-
 /**
+ * print_alphabet_x10 - print all lowercase character 10times
  *
- *  * print_alphabet_x10 - print all lowercase character 10times
+ * Description: using nested while-for loop
  *
- *   *
- *
- *    * Description: using nested while-for loop
- *
- *     *
- *
- *      * Return: void
- *
- *       */
-
+ * Return: void
+ */
 void print_alphabet_x10(void)
-
 {
+	char alpha;
+	int i;
 
-		char alpha;
-
-			int i;
-
-
-
-				i = 0;
-
-					while (i <= 9)
-
-							{
-
-										for (alpha = 'a'; alpha <= 'z'; alpha++)
-
-													{
-
-																	_putchar(alpha);
-
-																			}
-
-												_putchar('\n');
-
-														i++;
-
-															}
-
+	i = 0;
+	while (i <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
