e0a269c112c048848e33d12240091feb82c23cef
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-13 15:18:34 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,61 +1,31 @@
 #include <unistd.h>
-
 #include "main.h"
 
-
-
 /**
+ * print_sign- Entry point
  *
- *  * print_sign- Entry point
+ * @n: integer parameter
  *
- *   *
+ * Description: Check sign of number
  *
- *    * @n: integer parameter
- *
- *     *
- *
- *      * Description: Check sign of number
- *
- *       *
- *
- *        * Return: Always 1 or 0 (success)
- *
- *         */
-
+ * Return: Always 1 or 0 (success)
+ */
 int print_sign(int n)
-
 {
+	int ret_val;
 
-		int ret_val;
-
-
-
-			if (n > 0)
-
-					{
-
-								_putchar('+');
-
-										ret_val = 1;
-
-											} else if (n == 0)
-
-													{
-
-																_putchar('0');
-
-																		ret_val = 0;
-
-																			} else
-
-																					{
-
-																								_putchar('-');
-
-																										ret_val = -1;
-
-																											}
-
-																				return (ret_val);
-
+	if (n > 0)
+	{
+		_putchar('+');
+		ret_val = 1;
+	} else if (n == 0)
+	{
+		_putchar('0');
+		ret_val = 0;
+	} else
+	{
+		_putchar('-');
+		ret_val = -1;
+	}
+	return (ret_val);
 }
