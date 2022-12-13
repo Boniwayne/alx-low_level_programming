b9e238c3c3826ca3369043431cda461bc496e8b9
Boniwayne <Boniwayne125@gmail.com>
2022-12-13 04:40:19 -0800

    Added tasj 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,69 @@
+#include <unistd.h>
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * times_table- Entry point
+ *
+ *   *
+ *
+ *    * Description: Print 9 times table
+ *
+ *     *
+ *
+ *      * Return: Always 0 (success)
+ *
+ *       */
+
+void times_table(void)
+
+{
+
+		int i, j, times;
+
+
+
+			for (i = 0; i <= 9; i++)
+
+					{
+
+								_putchar('0');
+
+										for (j = 1; j <= 9; j++)
+
+													{
+
+																	times = i * j;
+
+																				_putchar(',');
+
+																							_putchar(' ');
+
+																										if (times <= 9)
+
+																														{
+
+																																			_putchar(' ');
+
+																																							_putchar(times + 48);
+
+																																										} else
+
+																																														{
+
+																																																			_putchar((times / 10) + 48);
+
+																																																							_putchar((times % 10) + 48);
+
+																																																										}
+
+																																												}
+
+												_putchar('\n');
+
+													}
+
+}
