b92cee7e5fa94db94b7a5ac4a8f7cbf2cf8101d5
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-13 16:42:56 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,49 +1,25 @@
 #include <stdio.h>
 
-
-
 /**
+ * main- Entry point
  *
- *  * main- Entry point
+ * Description: Print the sum of all natural numbers
+ * within range 0 - 1024 that are multiples of 3 or 5
  *
- *   *
- *
- *    * Description: Print the sum of all natural numbers
- *
- *     * within range 0 - 1024 that are multiples of 3 or 5
- *
- *      *
- *
- *       * Return: Always 0 (success)
- *
- *        */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
+	int i, sum;
 
-		int i, sum;
-
-
-
-			sum = 0;
-
-				for (i = 0; i < 1024; i++)
-
-						{
-
-									if (i % 3 == 0 || i % 5 == 0)
-
-												{
-
-																sum += i;
-
-																		}
-
-										}
-
-					printf("%d\n", sum);
-
-						return (0);
-
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }
