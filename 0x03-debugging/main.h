a6c0c6589fd864733479813201e110ac1b541166
Boniwayne <Boniwayne125@gmail.com>
2022-12-14 00:30:35 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,39 +1,9 @@
-#ifndef MAIN_H                                                                                        
-#define MAIN_H                                                                                        
-                                                                                                      
-/**                                                                                                   
- *File: main.h                                                                                 
- *Description: contains all functions prototype used in this project
- **/                                                                                                   
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet(void);                                                                            
-
-void print_alphabet_x10(void);                                                                        
-
-int _putchar(char c);                                                                                 
-
-int _islower(int c);                                                                                  
-
-int _isalpha(int c);                                                                                  
-
-int print_sign(int n);                                                                                
-
-int _abs(int);                                                                                        
-
-int print_last_digit(int);                                                                            
-
-void jack_bauer(void);                                                                                
-
-void times_table(void);                                                                               
-
-int add(int, int);                                                                                    
-
-void print_to_98(int n);                                                                              
-
-void print_times_table(int n);                                                                        
-
-int largest_number(int a, int b, int c);                                                                          
-
-                                                                                                      
-
-#endif /* MAIN_H */      i
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */i
