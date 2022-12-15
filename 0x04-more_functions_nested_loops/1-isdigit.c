3d9a8930d80bcc3bf86f7efc1d4f5ff48b2aaf91
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-15 16:59:54 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,24 +1,20 @@
 #include <unistd.h>
-
 #include <ctype.h>
-
 #include <stdio.h>
 
 /**
- * main - checks if  for a digit (0 through 9).
- *  @c: character input
- * Return 1 if c is a digit otherwise return 0
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character input
+ *
+ * Return: 1 if c is uppercase 0 otherwise
  */
 int _isdigit(int c)
 {
+	int ret_val;
 
-	int dig;
-
-	if(isdigit(c))
-		
-		dig = 1;
+	if (isdigit(c))
+		ret_val = 1;
 	else
-		dig = 0;
-
-	return dig;
+		ret_val = 0;
+	return (ret_val);
 }
