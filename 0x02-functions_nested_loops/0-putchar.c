ff67a7ccf3943a49b705c6c3f2036c9be18ee7e6
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-15 22:28:31 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main- Entry point
- *
- * Description: Print character using _putchar
- *
- * Return: Always 0 (success)
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch_arr[8] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 7; i++)
-	{
-		_putchar(ch_arr[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
