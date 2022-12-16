3db66726fd3b857503d4b74046de5991bcd8a127
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-16 04:07:29 +0300

    Update 1-main.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
-
-
 /**
+ * main - check the code
  *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
-
+ * Return: Always 0.
+ */
 int main(void)
-
 {
-
-	    print_alphabet();
-
-	        return (0);
-
+    print_alphabet();
+    return (0);
 }
