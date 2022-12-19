4dac75d4af6cbe899bd030793767b42608e08aa0
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-19 21:28:49 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,45 +1,23 @@
-nclude"main.h"
-
-
+#include"main.h"
 
 /**
+ * print_rev - prints a string in reverse
  *
- *  * print_rev - prints a string in reverse
+ * @s: string parameter input
  *
- *   *
- *
- *    * @s: string parameter input
- *
- *     *
- *
- *      * Return: Nothing
- *
- *      */
-
-
+ * Return: Nothing
+*/
 
 void print_rev(char *s)
-
 {
+	int index;
 
-		int index;
+	/*finds the length of string without null character*/
+	for (index = 0; s[index] != '\0'; ++index)
+		;
 
-
-
-			/*finds the length of string without null character*/
-
-			for (index = 0; s[index] != '\0'; ++index)
-
-						;
-
-
-
-				/*print char from the last index as you decrement*/
-
-				for (--index; index >= 0; --index)
-
-							_putchar(s[index]);
-
-					_putchar('\n');
-
+	/*print char from the last index as you decrement*/
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
 }
