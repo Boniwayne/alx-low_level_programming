bede911c697134b8277cce05ee3ae219a98ba785
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-19 21:11:15 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,33 +1,17 @@
 /**
+ * _strlen - returns the length of a string
  *
- *  * _strlen - returns the length of a string
+ * @s: string parameter input
  *
- *   *
- *
- *    * @s: string parameter input
- *
- *     *
- *
- *      * Return: length of string
- *
- *      */
-
-
+ * Return: length of string
+*/
 
 int _strlen(char *s)
-
 {
+	int counter;
 
-		int counter;
+	for (counter = 0; *s != '\0'; ++s)
+		++counter;
 
-
-
-			for (counter = 0; *s != '\0'; ++s)
-
-						++counter;
-
-
-
-				return (counter);
-
+	return (counter);
 }
