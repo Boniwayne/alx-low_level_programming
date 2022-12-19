df2818bdf43aa64f805e03bc0001b463d2a9d6b3
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-19 21:17:39 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,37 +1,19 @@
 #include"main.h"
 
-
-
 /**
+ * _puts - prints a string, followed by a newline to stdout
  *
- *  * _puts - prints a string, followed by a newline to stdout
+ * @str: string parameter to print
  *
- *   *
- *
- *    * @str: string parameter to print
- *
- *     *
- *
- *      * Return: Nothing
- *
- *      */
-
-
+ * Return: Nothing
+*/
 
 void _puts(char *str)
-
 {
-
-		while (*str != '\0')
-
-				{
-
-							_putchar(*str + 0);
-
-									++str;
-
-										}
-
-			_putchar('\n');
-
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
 }
