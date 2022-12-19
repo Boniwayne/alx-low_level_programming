dfea4f0a13de73d44eab8cbd10c95feaa9a121e8
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-19 22:28:20 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,28 +1,28 @@
 #include"main.h"
 
 /**
- *  * puts_half - prints half of a string
- *   *
- *    * @str: string parameter input
- *     *
- *      * Return: Nothing
- *      */
+ * puts_half - prints half of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing
+*/
 
 void puts_half(char *str)
 {
-		int l, n;
+	int l, n;
 
-			for (l = 0; str[l] != '\0'; ++l)
-						;
+	for (l = 0; str[l] != '\0'; ++l)
+		;
 
-				if (l % 2 == 0)
-						{
-									for (n = l / 2; str[n] != '\0'; ++n)
-													_putchar(str[n]);
-										} else
-												{
-															for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
-																			_putchar(str[n]);
-																}
-											_putchar('\n');
+	if (l % 2 == 0)
+	{
+		for (n = l / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	} else
+	{
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
 }
