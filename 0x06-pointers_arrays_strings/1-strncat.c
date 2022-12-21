52f716b166cad24902a21e7b30261ee7b11c3ba4
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2022-12-21 10:43:07 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,61 +1,31 @@
 /**
+ * _strncat - a function that concatenates two strings.
  *
- *  * _strncat - a function that concatenates two strings.
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
  *
- *   *
- *
- *    * @dest: pointer to destination input
- *
- *     * @src: pointer to source input
- *
- *      * @n: most number of bytes from @src
- *
- *       *
- *
- *        * Return: @dest
- *
- *        */
-
-
+ * Return: @dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
-
 {
+	int c, i;
 
-		int c, i;
+	c = 0;
 
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
 
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
 
-			c = 0;
-
-
-
-				/*find size of dest array*/
-
-				while (dest[c])
-
-							c++;
-
-
-
-					/**
-					 *
-					 * 	 * src does not need to be null terminated
-					 *
-					 * 	 	 * if it contains n or more bytes
-					 *
-					 * 	 	 	*/
-
-					for (i = 0; i < n && src[i] != '\0'; i++)
-
-								dest[c + i] = src[i];
-
-						/*null terminate dest*/
-
-						dest[c + i] = '\0';
-
-
-
-							return (dest);
-
+	return (dest);
 }
