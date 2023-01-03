f1fa1c5618634eb766cc6885ab4fd93c0941e556
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-03 21:56:00 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,71 +1,36 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_chessboard - print the chessboard, 2d array
- *
- *   * @a: 2d array of chars
- *
- *    * Return: void
- *
- *     */
-
+ * print_chessboard - print the chessboard, 2d array
+ * @a: 2d array of chars
+ * Return: void
+ */
 void print_chessboard(char (*a)[8])
-
 {
-
-		/*variables*/
-
-		int n, j;
+	/*variables*/
+	int n, j;
 
 
 
+	n = 0;
 
+	while (n < 8) /*repeat number*/
 
+	{
 
+		j = 0;
 
-			n = 0;
+		while (j < 8)
 
+		{
 
+			_putchar (a[n][j]);
 
-				while (n < 8) /*repeat number*/
+			j++;
 
+		}
 
-
-						{
-
-
-
-									j = 0;
-
-
-
-											while (j < 8)
-
-
-
-														{
-
-
-
-																		_putchar (a[n][j]);
-
-
-
-																					j++;
-
-
-
-																							}
-
-
-
-													_putchar ('\n'); /*print new line*/
-
-															n++;
-
-																}
-
+		_putchar ('\n'); /*print new line*/
+		n++;
+	}
 }
