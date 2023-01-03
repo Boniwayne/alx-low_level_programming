470daecffbe736a1323623d643cb813479aa0d3e
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-03 21:46:39 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,60 +1,30 @@
 /**
+ * _strpbrk - a function that searches a
+ *            string for any of a set of bytes.
  *
- *  * _strpbrk - a function that searches a
+ * @s: pointer to input string
+ * @accept: pointer to string we
+ *         searching for in @s
  *
- *   *            string for any of a set of bytes.
- *
- *    *
- *
- *     * @s: pointer to input string
- *
- *      * @accept: pointer to string we
- *
- *       *         searching for in @s
- *
- *        *
- *
- *         * Return: pointer to the bytes in @s
- *
- *          *         or NULL if no such byte is found
- *
- *          */
-
-
+ * Return: pointer to the bytes in @s
+ *         or NULL if no such byte is found
+*/
 
 char *_strpbrk(char *s, char *accept)
-
 {
+	int i, j;
 
-		int i, j;
-
-
-
-			i = 0;
-
-				while (s[i] != '\0')
-
-						{
-
-									j = 0;
-
-											while (accept[j] != '\0')
-
-														{
-
-																		if (s[i] == accept[j])
-
-																							return (s + i);
-
-																					j++;
-
-																							}
-
-													i++;
-
-														}
-
-					return ('\0');
-
-}:wq
-
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
