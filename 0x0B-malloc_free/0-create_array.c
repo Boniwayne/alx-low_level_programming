609d212f2b8fbc70c6e8cb434fbaafe1a6994dbf
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-11 16:17:59 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,67 +1,34 @@
 #include <stdlib.h>
-
 #include "main.h"
 
-
-
 /**
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
  *
- *  * *create_array - creates an array of chars,
- *
- *   * and initializes it with a specific char
- *
- *    * @size: size of the array to create
- *
- *     * @c: char to initialize the array c
- *
- *      *
- *
- *       * Return: pointer to the array (Success), NULL (Error)
- *
- *        */
-
+ * Return: pointer to the array (Success), NULL (Error)
+ */
 char *create_array(unsigned int size, char c)
-
 {
+	char *p;
+	unsigned int i = 0;
 
-		char *p;
+	if (size == 0)
+		return (NULL);
 
-			unsigned int i = 0;
+	p = (char *) malloc(sizeof(char) * size);
 
+	if (p == NULL)
+		return (0);
 
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
 
-				if (size == 0)
+	*(p + i) = '\0';
 
-							return (NULL);
-
-
-
-					p = (char *) malloc(sizeof(char) * size);
-
-
-
-						if (p == NULL)
-
-									return (0);
-
-
-
-							while (i < size)
-
-									{
-
-												*(p + i) = c;
-
-														i++;
-
-															}
-
-
-
-								*(p + i) = '\0';
-
-
-
-									return (p);
-
+	return (p);
 }
