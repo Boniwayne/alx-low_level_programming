66f1c042033b28ea38a4beaff60fd9ebac6dd248
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-13 12:29:57 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,135 +1,68 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include <stdio.h>
 
 
-
-
-
 void *malloc_checked(unsigned int b);
-
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-
 void *_calloc(unsigned int nmemb, unsigned int size);
-
 int *array_range(int min, int max);
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 int _putchar(char c);
-
-
 
 /*functions for 101-mul.c */
 
-
-
 /**
+ * error_exit - prints error with _putchar
+ *              and exits with 98
  *
- *  * error_exit - prints error with _putchar
- *
- *   *              and exits with 98
- *
- *    *
- *
- *     * Return: Error 98 and exit(98)
- *
- *     */
-
-
+ * Return: Error 98 and exit(98)
+*/
 
 int error_exit(void)
-
 {
+	char *err;
+	int i;
 
-		char *err;
-
-			int i;
-
-
-
-				err = "Error";
-
-					for (i = 0; err[i] != '\0'; i++)
-
-								_putchar(err[i]);
-
-						_putchar('\n');
-
-							exit(98);
-
+	err = "Error";
+	for (i = 0; err[i] != '\0'; i++)
+		_putchar(err[i]);
+	_putchar('\n');
+	exit(98);
 }
 
-
-
 /**
+ * check_number - checks if string has only
+ *                numbers
  *
- *  * check_number - checks if string has only
+ * @str: string to check
  *
- *   *                numbers
- *
- *    *
- *
- *     * @str: string to check
- *
- *      *
- *
- *       * Return: 0 is true 1 if false
- *
- *       */
-
-
+ * Return: 0 is true 1 if false
+*/
 
 int check_number(char *str)
-
 {
-
-		while (*str != '\0')
-
-				{
-
-							if (*str < '0' || *str > '9')
-
-											return (1);
-
-									str++;
-
-										}
-
-			return (0);
-
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (1);
+		str++;
+	}
+	return (0);
 }
 
-
-
 /**
+ * _length - get the length of strings
  *
- *  * _length - get the length of strings
+ * @str: string to get length of
  *
- *   *
- *
- *    * @str: string to get length of
- *
- *     *
- *
- *      * Return: length of string
- *
- *      */
-
-
+ * Return: length of string
+*/
 
 int _length(char *str)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (str[i] != '\0')
-
-						i++;
-
-				return (i);
-
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
