142309ed6446bc3c5fc053f97653ebf041856235
Boniwayne <Boniwayne125@gmail.com>
2023-01-17 05:06:30 -0800

    Added taskt

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-#define MUM main.c
+/**
+ *main - prints the  name of file name
+ *
+ * Return: 0 Always
+ */
+int main(void){
 
-int main(void)
-{
-	printf(%d\n, MUM);
+	char *file_name;
+
+	file_name =  _FILE_;
+	
+	printf("%s\n", file_name);
+
 
 	return (0);
 
