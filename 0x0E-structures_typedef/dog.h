150e4a5def12954c1f938281a5d3113155cd6d55
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-17 22:11:22 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,23 @@
 #define DOG_H
 
 /**
- *  * struct dog - dog structure
- *   *
- *    * @name: name of dog
- *     * @age: age of the dog
- *      * @owner: dog owner
- *       *
- *       */
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
- *  * dog_t -Typedef for struct dog
- *  */
+ * dog_t -Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
