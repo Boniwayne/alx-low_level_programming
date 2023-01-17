491e48054ca61a98d38e5b9765a2e52c89b92bb9
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-17 22:36:54 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- *  * print_dog - print dog
- *   *
- *    * @d: pointer to struct dog
- *     *
- *      * Return: nothing
- *      */
+ * print_dog - print dog
+ *
+ * @d: pointer to struct dog
+ *
+ * Return: nothing
+*/
 
 void print_dog(struct dog *d)
 {
-		if (d != NULL)
-				{
-							printf("Name: ");
-									d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
-											printf("Age: %f\n", d->age);
-													printf("Owner: ");
-															d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
-																}
+	if (d != NULL)
+	{
+		printf("Name: ");
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: ");
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
+	}
 }
