917e8e6d23ae618c651a37f46b709707b50dc2fb
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-19 23:58:14 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,33 +2,33 @@
 #include <stddef.h>
 
 /**
- *  * get_op_func - get ops function pointer of type char array
- *   *               that accepts two inputs of int data type
- *    *
- *     * @s: a character pointer pointing to a symbol from the program argument
- *      *
- *       * Return: one of the operator functions to perform calculations
- *       */
+ * get_op_func - get ops function pointer of type char array
+ *               that accepts two inputs of int data type
+ *
+ * @s: a character pointer pointing to a symbol from the program argument
+ *
+ * Return: one of the operator functions to perform calculations
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
-		/* struct opts of struct op_t */
-		op_t ops[] = {
-					{"+", op_add},
-							{"-", op_sub},
-									{"*", op_mul},
-											{"/", op_div},
-													{"%", op_mod},
-															{NULL, NULL}
-						};
-			int i = 0;
+	/* struct opts of struct op_t */
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-				while (i < 5)
-						{
-									if (*s == *ops[i].op)
-													return (ops[i].f);
-											i++;
-												}
+	while (i < 5)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
 
-					return (NULL);
+	return (NULL);
 }
