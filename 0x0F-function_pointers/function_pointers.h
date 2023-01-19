e6a8e915bc085e200760ee88c8536c3853cdb75c
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-19 23:42:49 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,14 +6,14 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 /**
- *  * enum bool - typedef function for bool
- *   * @TRUE: 1
- *    * @FALSE: 0
- *    */
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+*/
 typedef enum bool
 {
-		TRUE = 1,
-			FALSE = 0
+	TRUE = 1,
+	FALSE = 0
 } bool;
 
 #endif /* FUNCTION_POINTERS_H */
