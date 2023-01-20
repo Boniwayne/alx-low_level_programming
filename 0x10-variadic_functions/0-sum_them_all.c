39c0a99d7703e6dbcf52f5a84a4878579c82f2ce
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-01-20 10:28:31 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,53 +1,27 @@
 #include <stdarg.h>
 
-
-
 /**
+ * sum_them_all - adds all its parameters
  *
- *  * sum_them_all - adds all its parameters
+ * @n: start of input variables
  *
- *   *
- *
- *    * @n: start of input variables
- *
- *     *
- *
- *      * Return: the sum
- *
- *      */
-
-
+ * Return: the sum
+*/
 
 int sum_them_all(const unsigned int n, ...)
-
 {
+	va_list ap;
+	unsigned int param, sum = 0;
 
-		va_list ap;
+	/* initialize the argument list from the start */
+	va_start(ap, n);
 
-			unsigned int param, sum = 0;
+	/* iterate through all parameter values*/
+	for (param = 0; param < n; param++)
+		/* get the next parameter value and add it to sum*/
+		sum += va_arg(ap, int);
+	/*Clean up*/
+	va_end(ap);
 
-
-
-				/* initialize the argument list from the start */
-
-				va_start(ap, n);
-
-
-
-					/* iterate through all parameter values*/
-
-					for (param = 0; param < n; param++)
-
-								/* get the next parameter value and add it to sum*/
-
-								sum += va_arg(ap, int);
-
-						/*Clean up*/
-
-						va_end(ap);
-
-
-
-							return (sum);
-
+	return (sum);
 }
