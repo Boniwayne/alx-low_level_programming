0e0730d0faba806e25d2c918489147785494f461
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-02-01 16:56:33 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,41 +1,21 @@
 #include "lists.h"
 
-
-
 /**
+ * free_listint - free list
  *
- *  * free_listint - free list
+ * @head: pointer to the first node of the list
  *
- *   *
- *
- *    * @head: pointer to the first node of the list
- *
- *     *
- *
- *      * Return: nothing
- *
- *      */
-
+ * Return: nothing
+*/
 void free_listint(listint_t *head)
-
 {
+	listint_t *current;
 
-		listint_t *current;
-
-
-
-			/*iterate through each node in list*/
-
-			while ((current = head) != NULL)
-
-					{
-
-								/*set head as the next node*/
-
-								head = head->next;
-
-										free(current);
-
-											}
-
+	/*iterate through each node in list*/
+	while ((current = head) != NULL)
+	{
+		/*set head as the next node*/
+		head = head->next;
+		free(current);
+	}
 }
