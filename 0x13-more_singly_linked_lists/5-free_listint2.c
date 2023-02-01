74907151cd1177828a5b01fd873febad95dad115
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-02-01 16:57:56 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,61 +1,31 @@
 #include "lists.h"
 
-
-
 /**
+ * free_listint2 - a function that frees the list and sets head to NULL
  *
- *  * free_listint2 - a function that frees the list and sets head to NULL
+ * @head: pointer to the first node
  *
- *   *
- *
- *    * @head: pointer to the first node
- *
- *     *
- *
- *      * Return: nothing
- *
- *      */
-
+ * Return: nothing
+*/
 void free_listint2(listint_t **head)
-
 {
+	listint_t *current, *temp;
 
-		listint_t *current, *temp;
+	if (head != NULL)
+	{
+		/*set head addr to current*/
+		current = *head;
 
+		/*iterate through the whole list*/
+		/*while setting the current node to temp*/
+		while ((temp = current) != NULL)
+		{
+			/*set next node to curretnt*/
+			current = current->next;
+			/*free temp, that is the current node*/
+			free(temp);
+		}
 
-
-			if (head != NULL)
-
-					{
-
-								/*set head addr to current*/
-
-								current = *head;
-
-
-
-										/*iterate through the whole list*/
-
-										/*while setting the current node to temp*/
-
-										while ((temp = current) != NULL)
-
-													{
-
-																	/*set next node to curretnt*/
-
-																	current = current->next;
-
-																				/*free temp, that is the current node*/
-
-																				free(temp);
-
-																						}
-
-
-
-												*head = NULL;
-
-													}
-
+		*head = NULL;
+	}
 }
