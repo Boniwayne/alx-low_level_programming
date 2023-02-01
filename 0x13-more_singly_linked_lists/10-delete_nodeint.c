296f18b8e3e8ee84ff973276fa34a0550dfa6fb4
Bonface Ochieng <114133981+Boniwayne@users.noreply.github.com>
2023-02-01 17:05:21 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,93 +1,47 @@
 #include "lists.h"
 
-
-
 /**
+ * delete_nodeint_at_index - deletes the node at index
+ * of a linked list.
+ * @head: head of a list.
+ * @index: index of the list where the node is
+ * deleted.
  *
- *  * delete_nodeint_at_index - deletes the node at index
- *
- *   * of a linked list.
- *
- *    * @head: head of a list.
- *
- *     * @index: index of the list where the node is
- *
- *      * deleted.
- *
- *       *
- *
- *        * Return: 1 if it succeeded, -1 if it failed.
- *
- *         */
-
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
-
 {
+	unsigned int i;
+	listint_t *prev;
+	listint_t *next;
 
-		unsigned int i;
+	prev = *head;
 
-			listint_t *prev;
+	if (index != 0)
+	{
+		for (i = 0; i < index - 1 && prev != NULL; i++)
+		{
+			prev = prev->next;
+		}
+	}
 
-				listint_t *next;
+	if (prev == NULL || (prev->next == NULL && index != 0))
+	{
+		return (-1);
+	}
 
+	next = prev->next;
 
+	if (index != 0)
+	{
+		prev->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(prev);
+		*head = next;
+	}
 
-					prev = *head;
-
-
-
-						if (index != 0)
-
-								{
-
-											for (i = 0; i < index - 1 && prev != NULL; i++)
-
-														{
-
-																		prev = prev->next;
-
-																				}
-
-												}
-
-
-
-							if (prev == NULL || (prev->next == NULL && index != 0))
-
-									{
-
-												return (-1);
-
-													}
-
-
-
-								next = prev->next;
-
-
-
-									if (index != 0)
-
-											{
-
-														prev->next = next->next;
-
-																free(next);
-
-																	}
-
-										else
-
-												{
-
-															free(prev);
-
-																	*head = next;
-
-																		}
-
-
-
-											return (1);
-
+	return (1);
 }
